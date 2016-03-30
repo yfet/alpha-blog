7a16d0e449dac2eea15c8f8d049ead666f7e0ff1
yfet <tefy.nahary@gmail.com>
2016-03-30 20:31:37 +0300

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-root 'pages#home'
-get 'about', to: 'pages#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20160329171722) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
+    t.string "title"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
