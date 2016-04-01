1c5b534bc36c30d7b794f46f59cb406b0f0e2d76
yfet <tefy.nahary@gmail.com>
2016-04-01 19:56:47 +0300

    style form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:notice] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:notice] = 'Article was successfully updated'
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -35,12 +35,13 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article.destroy();
-    flash[:notice] = "Article was successfully deleted"
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
   private
+
   def set_article
     @article = Article.find(params[:id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329171722) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+ActiveRecord::Schema.define(version: 20_160_329_171_722) do
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
-
 end
