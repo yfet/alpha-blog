c238e7e0cc6e63a189d676d0ea5f897b3548b8b1
yfet <tefy.nahary@gmail.com>
2016-04-04 20:11:28 +0300

    Add user data to articles listing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # User Model
 class User < ActiveRecord::Base
+  has_many :articles
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
